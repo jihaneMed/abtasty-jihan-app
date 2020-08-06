0f7b472fcc8fb77c2d75bab479510da5f6d93170
jihan.elmedini <jihan.elmedini@abtasty.com>
2020-08-06 16:08:01 +0200

    changes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Stories from "./Stories";
 
 function App() {
+  const [userQuery, setUserQuery] = useState("");
+
+  const updateUserQuery = event => {
+    console.log("userQuery", userQuery);
+    setUserQuery(event.target.value);
+  };
+
+  const handleKeyPress = event => {
+    if (event.key === "Enter") {
+      searchQuery();
+    }
+  };
+
+  const searchQuery = () => {
+    window.open(`https://google.com/search?q=${userQuery}`, "_blank");
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello Jihan, What's on your mind ?</h1>
+      <div className="form">
+        <input
+          value={userQuery}
+          onChange={updateUserQuery}
+          onKeyPress={handleKeyPress}
+        />
+        <button onClick={searchQuery}>Search</button>
+      </div>
+      <hr />
+      <hr />
+      <Stories />
     </div>
   );
 }
-
 export default App;
