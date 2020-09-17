535be4ec7a164554d418177c00fbf9ec4ff4cd36
jihan.elmedini <jihan.elmedini@abtasty.com>
2020-09-17 18:35:19 +0200

    tr

--- src/Booking.js
+++ src/Booking.js
@@ -1,30 +1,8 @@
-import React, {useEffect} from "react";
+import React from "react";
 
 
 
 function Booking() {
-    
-    useEffect(() => {
-        const transId = Math.round(Math.random()*100000).toString();
-        window.abtasty.send("transaction", { 
-            tid: transId,
-            ta: "checkoutId",
-            pm: "paypal",
-            sm: "relais colis",
-            tc: "EUR",
-            tr: 1150,
-            icn: 1,
-        });
-        window.abtasty.send("item", {
-            tid: transId,
-            ic: "Code1",
-            in: "Séjour",
-            iv: "CAT1",
-            iq: 1,
-            ip: 1000,
-        });
-    }, []);
-
     return(
         <div>
             {'You trip is Booked !'}
